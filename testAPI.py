3a52ab6b360727bde69e23d7e61cd98603b4e94a
Nathalie Souza <69611410+nathsouza@users.noreply.github.com>
2022-10-13 11:00:18 -0400

    Update testAPI.py

--- testAPI.py
+++ testAPI.py
@@ -10,7 +10,10 @@ import gitHubAPI
 class TestAPI(unittest.TestCase):
 
     def testAPIs(self):
-        # self.assertEqual(gitHubAPI.gitHubAPI.(''), [])
+        test =  gitHubAPI.gitHubAPI("nathsouza") 
+        actual = {'GitHubAPI567':17, 'SSW567-HW2A-Triangle': 21, 'SSW567-HW1-Triangle': 3, 'stevens-ssw567-fall2022': 3, 'String-Calculator': 2, 'nathsouza.github.io': 8, 'Oenofiles': 6, 'Python': 1, 'python_project_week': 1, 'python_project_week-1': 1, 'mock_project' : 2}
+        self.assertDictEqual(test, actual)
+        self.assertEqual(len(test.values()), 12)
 
 if __name__ == '__main__':
     unittest.main()
